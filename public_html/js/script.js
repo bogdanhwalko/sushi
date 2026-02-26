e90bdf6ecbf141296752e27048ac628c3c855ca3
bogdanhwalko <36229866+bogdanhwalko@users.noreply.github.com>
2026-02-26 14:07:10 +0200

    default image

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -552,3 +552,15 @@ function showToast(message) {
     const toast = bootstrap.Toast.getOrCreateInstance(toastEl, {delay: 2200});
     toast.show();
 }
+
+document.addEventListener('error', function(e) {
+    const img = e.target;
+
+    if (img.tagName === 'IMG') {
+
+        if (img.dataset.fallback === '1') return;
+
+        img.dataset.fallback = '1';
+        img.src = '/images/products/no_image.png'; // ІСНУЮЧИЙ файл!
+    }
+}, true);
